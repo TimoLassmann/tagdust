9d34cbd8f3b103844eaf6d0f46e9464eb7e28a7d
lassmann <lassmann@tkx424.genome.gsc.riken.jp>
2013-02-05 11:32:15 +0900

    added gnu

--- src/io.h
+++ src/io.h
@@ -1,15 +1,15 @@
 /*
  
- Copyright (C) 2010 Timo Lassmann <timolassmann@gmail.com>
+ Copyright (C) 2013 Timo Lassmann <timolassmann@gmail.com>
  
- This file is part of SAMstat.
+ This file is part of TagDust.
  
- Delve is free software: you can redistribute it and/or modify
+ TagDust is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
  
- Delve is distributed in the hope that it will be useful,
+ TagDust is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
@@ -18,6 +18,7 @@
  along with Delve.  If not, see <http://www.gnu.org/licenses/>.
  
  */
+
 
 
 struct read_info{
